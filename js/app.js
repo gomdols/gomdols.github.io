840b7a624e4247cc1be98aa7c40e3314185063c3
20402김재현 <59628836+gomdols@users.noreply.github.com>
2021-07-11 20:02:27 +0900

    sign in/out

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const signinForm = document.querySelector("#signin-form");
+const signinInput = signinForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+const changeUsername = document.querySelector(".change-username");
+const clock = document.querySelector(".clock")
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function onSigninSubmit(event) {
+    event.preventDefault();
+    signinForm.classList.add(HIDDEN_CLASSNAME);
+    const username = signinInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+if (savedUsername === null) {
+    signinForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+    paintGreetings(savedUsername)
+}
+
+signinForm.addEventListener("submit", onSigninSubmit);
+
+function onChangeUsername(event){
+    event.preventDefault();
+    signinInput.value = savedUsername;
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    changeUsername.classList.add(HIDDEN_CLASSNAME);
+    clock.classList.add(HIDDEN_CLASSNAME);
+    signinForm.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.removeItem(USERNAME_KEY);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    changeUsername.classList.remove(HIDDEN_CLASSNAME);
+    clock.classList.remove(HIDDEN_CLASSNAME)
+}
+
+changeUsername.querySelector("a").addEventListener("click", onChangeUsername);
